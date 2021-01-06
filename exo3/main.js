32a6ca5cb829798789b1d1c363ed264d67fd7bd2
FloLaffargue <florian.laffargue@laposte.net>
2021-01-06 10:57:12 +0100

    demo

--- exo3/main.js
+++ exo3/main.js
@@ -1,13 +1,16 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
-    alert(event)
-    var absolute = event.absolute;
-    var alpha    = event.alpha;
-    var beta     = event.beta;
-    var gamma    = event.gamma;
-  
-    // Faites quelque chose avec les données acquises. ;)
-
+    
+    document.querySelector('#orientation').textContent = event
+    displayElement("#x", event.gamma)
+    displayElement("#y", event.beta)
+    displayElement("#z", event.alpha)
+    displayElement("#absolute", event.alpha)
   }
+
+  function displayElement(id, content) {
+    if(content == null) content = 0
+    document.querySelector(id).textContent = content
+}
   
