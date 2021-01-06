c1d6dc77815bd4767f64f6579eb39d90cad3198e
FloLaffargue <florian.laffargue@laposte.net>
2021-01-06 19:01:45 +0100

    fix

--- TP1/exo3/main.js
+++ TP1/exo3/main.js
@@ -1,27 +1,35 @@
-window.addEventListener("deviceorientation", handleOrientation, true);
+var rep = prompt("orientation")
 
-function handleOrientation(event) {
-    
-    document.querySelector('#orientation').textContent = event
-    displayElement("#xO", event.gamma)
-    displayElement("#yO", event.beta)
-    displayElement("#zO", event.alpha)
+if(rep == "o") {
+  window.addEventListener("deviceorientation", handleOrientation, true);
+
+  function handleOrientation(event) {
+      
+      document.querySelector('#orientation').textContent = event
+      displayElement("#xO", event.gamma)
+      displayElement("#yO", event.beta)
+      displayElement("#zO", event.alpha)
   }
 
-
+} else {
+  
   window.addEventListener("devicemotion", handleMotion)
 
   function handleMotion(event) {
     displayElement("#xM", event.acceleration.x)
     displayElement("#yM", event.acceleration.y)
     displayElement("#zM", event.acceleration.z)
-
+  
     displayElement("#xR", event.rotationRate.beta)
     displayElement("#yR", event.rotationRate.gamma)
     displayElement("#zR", event.rotationRate.alpha)
   }
+    
   
-
   function displayElement(id, content) {
     document.querySelector(id).textContent = content
   }
+}
+
+
+
--- TP1/exo4/main.js
+++ TP1/exo4/main.js
@@ -1,6 +1,7 @@
 document.querySelector("canvas").addEventListener('touchstart', function(e) {
   e.preventDefault()
   const touche = e.touches[0]
+  displayElement('#event', e.type)
   displayElement("#x", touche.screenX)
   displayElement("#y", touche.screenY)
   
